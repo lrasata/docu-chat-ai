eaea2bfc7bc285d1a1a978b8813fc2f8717f3ae9
Liantsoa Rasata <rmliantsoa@gmail.com>
2026-01-11 11:49:05 +0100

    fix: backend module file_uploader

--- terraform/layers/backend/modules/api_gateway/main.tf
+++ terraform/layers/backend/modules/api_gateway/main.tf
@@ -36,9 +36,9 @@ resource "aws_apigatewayv2_authorizer" "cognito" {
 # GET /files - List all files for authenticated user
 resource "aws_apigatewayv2_integration" "list_files" {
   api_id             = aws_apigatewayv2_api.api.id
-  integration_type   = "AWS_LAMBDA"
+  integration_type   = "AWS_PROXY"
   integration_method = "POST"
-  integration_uri    = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_get_document_data_arn}/invocations"
+  integration_uri    = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_list_files_arn}/invocations"
 }
 
 resource "aws_apigatewayv2_route" "list_files" {
@@ -60,7 +60,7 @@ resource "aws_lambda_permission" "list_files" {
 # GET /files/{id} - Get specific file content
 resource "aws_apigatewayv2_integration" "get_file" {
   api_id             = aws_apigatewayv2_api.api.id
-  integration_type   = "AWS_LAMBDA"
+  integration_type   = "AWS_PROXY"
   integration_method = "POST"
   integration_uri    = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_get_file_arn}/invocations"
 
@@ -85,7 +85,7 @@ resource "aws_lambda_permission" "get_file" {
 # GET /documents/{id} - Get document metadata from DynamoDB
 resource "aws_apigatewayv2_integration" "get_document_data" {
   api_id             = aws_apigatewayv2_api.api.id
-  integration_type   = "AWS_LAMBDA"
+  integration_type   = "AWS_PROXY"
   integration_method = "POST"
   integration_uri    = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_get_document_data_arn}/invocations"
 
