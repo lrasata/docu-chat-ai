ad952e80d73739ee0cf0e4aa937fc8c6153b174e
Liantsoa Rasata <rmliantsoa@gmail.com>
2026-01-11 09:51:49 +0100

    feat: secrets management layer ci/cd pipeline test

--- terraform/secrets/secrets_manager.tf
+++ terraform/secrets/secrets_manager.tf
@@ -1,5 +1,5 @@
 data "aws_secretsmanager_secret" "docu_chat_ai_secrets" {
-  name = "${var.environment}/docu-chat-ai/secrets"
+  name = "${var.environment}/${var.app_id}/secrets"
 }
 
 data "aws_secretsmanager_secret_version" "docu_chat_ai_secret_values" {
@@ -9,5 +9,5 @@ data "aws_secretsmanager_secret_version" "docu_chat_ai_secret_values" {
 locals {
   google_client_id        = jsondecode(data.aws_secretsmanager_secret_version.docu_chat_ai_secret_values.secret_string)["GOOGLE_CLIENT_ID"]
   google_client_secret    = jsondecode(data.aws_secretsmanager_secret_version.docu_chat_ai_secret_values.secret_string)["GOOGLE_CLIENT_SECRET"]
-  file_upload_auth_secret = jsondecode(data.aws_secretsmanager_secret_version.docu_chat_ai_secret_values.secret_string)["API_GW_AUTH_SECRET"]
+  file_upload_auth_secret = jsondecode(data.aws_secretsmanager_secret_version.docu_chat_ai_secret_values.secret_string)["API_GW_FILE_UPLOAD_AUTH_SECRET"]
 }
--- terraform/secrets/variables.tf
+++ terraform/secrets/variables.tf
@@ -1,11 +1,13 @@
 variable "environment" {
   description = "The environment for the deployment (e.g., dev, staging, prod)"
   type        = string
-  default     = "staging"
+}
+
+variable "app_id" {
+  type = string
 }
 
 variable "region" {
   description = "The AWS region to deploy resources"
   type        = string
-  default     = "eu-central-1"
 }
